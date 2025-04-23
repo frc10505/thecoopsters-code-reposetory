bd6da8dcf7d5055cc1889ab22fd26c3f94509aa6
carolinef158 <cfurmanski158@gmail.com>
2025-04-23 17:01:40 -0400

    Evil sim
    More evil sim stuff

--- src/main/java/frc/team10505/robot/subsystems/AlgaeSubsystem.java
+++ src/main/java/frc/team10505/robot/subsystems/AlgaeSubsystem.java
@@ -2,55 +2,149 @@ package frc.team10505.robot.subsystems;
 
 import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.sim.SparkAbsoluteEncoderSim;
+import com.revrobotics.spark.SparkAbsoluteEncoder;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.AnalogEncoder;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class AlgaeSubsystem extends SubsystemBase {
 
-    private final int pivotMotorId = 0;
-    private final int intakeMotorId = 0;
+    private final int kAlgaeIntakeMotorID = 0;
+    private final int kAlgaePivotMotorID = 1;
 
     private final double startingAngle = 0.0;
+    private double pivotSetPoint = 90.0;
+    private double absoluteOffset = 180.0;
+    private double encoderValue;
+    private double simEncoder = startingAngle;
+
+    private final static int kPivotMotorCurrentLimit = 8;
+    private final static int kIntakeMotorCurrentLimit = 15;
+    private final static int pivotEncoderScale = 360;
+    private final static int pivotEncoderOffset = 0;
+
+
+
 
     /* Motor Ids */
-    private TalonFX pivotMotor = new TalonFX(pivotMotorId);
-    private TalonFX intakeMotor = new TalonFX(intakeMotorId);
+    private SparkMax pivotMotor = new SparkMax(kAlgaePivotMotorID, MotorType.kBrushless);
+    private SparkAbsoluteEncoder pivotEncoder = pivotMotor.getAbsoluteEncoder();
+    private SparkMax intakeMotor = new SparkMax(kAlgaeIntakeMotorID, MotorType.kBrushless);
+    private SparkMaxConfig pivotMotorConfig = new SparkMaxConfig();
+    private SparkMaxConfig intakeMotorConfig = new SparkMaxConfig();
+    private SparkMaxConfig simPivotMotorConfig = new SparkMaxConfig();
+    private SparkMaxConfig simIntakeMotorConfig = new SparkMaxConfig();
+
 
     /* PID FeedFoward */
     private PIDController pivotController = new PIDController(0, 0, 0);
     private ArmFeedforward pivotFeedForward = new ArmFeedforward(0, 0, 0, 0);
+    private PIDController simPivotController = new PIDController(0, 0, 0);
+    private ArmFeedforward simPivotFeedForward = new ArmFeedforward(0, 0, 0, 0);
 
     /* Fancy Sim stuff */
     private final Mechanism2d pivotMech = new Mechanism2d(2, 2.5);
     private MechanismRoot2d pivotRoot = pivotMech.getRoot("pivotRoot", 1, 1.25);
-    private MechanismLigament2d pivotViz = pivotRoot.append(new MechanismLigament2d("pivotViz", 0.75, 0));
-    private SingleJointedArmSim pivotSim = new SingleJointedArmSim(DCMotor.getKrakenX60(2), 80,
+    private MechanismLigament2d pivotViz = pivotRoot.append(new MechanismLigament2d("pivotViz", 0.75, startingAngle));
+    private SingleJointedArmSim pivotSim = new SingleJointedArmSim(DCMotor.getNEO(1), 80,
             SingleJointedArmSim.estimateMOI(0.305, 2), 0.305, Units.degreesToRadians(-110), Units.degreesToRadians(110),
-            true, startingAngle, Units.degreesToRadians(startingAngle));
+            true, Units.degreesToRadians(startingAngle));
+
+
+
 
     public AlgaeSubsystem() {
         if (Utils.isSimulation()) {
-            pivotMotor = new TalonFX(pivotMotorId);
-            intakeMotor = new TalonFX(intakeMotorId);
             pivotController = new PIDController(0, 0, 0);
             pivotFeedForward = new ArmFeedforward(0, 0, 0, 0);
+            
+            pivotMotor = new SparkMax(kAlgaePivotMotorID, MotorType.kBrushless);
+            intakeMotor = new SparkMax(kAlgaeIntakeMotorID, MotorType.kBrushless);
+            pivotMotorConfig = new SparkMaxConfig();
+            intakeMotorConfig = new SparkMaxConfig();
 
         } else {
-            pivotMotor = new TalonFX(pivotMotorId);
-            intakeMotor = new TalonFX(intakeMotorId);
+            pivotMotor = new SparkMax(kAlgaePivotMotorID, MotorType.kBrushless);
+            intakeMotor = new SparkMax(kAlgaeIntakeMotorID, MotorType.kBrushless);
             pivotController = new PIDController(0, 0, 0);
             pivotFeedForward = new ArmFeedforward(0, 0, 0, 0);
 
         }
+        // Pivot motor config
+        pivotMotorConfig.idleMode(IdleMode.kBrake);
+        pivotMotorConfig.smartCurrentLimit(kPivotMotorCurrentLimit,
+                kPivotMotorCurrentLimit);
+        pivotMotorConfig.absoluteEncoder.positionConversionFactor(pivotEncoderScale); // Angle encoder scale
+        pivotMotorConfig.absoluteEncoder.zeroOffset(pivotEncoderOffset); // Angle encoder offset
+        pivotMotor.configure(pivotMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+        // Intake motor config
+        intakeMotorConfig.idleMode(IdleMode.kBrake);
+        intakeMotorConfig.smartCurrentLimit(kIntakeMotorCurrentLimit,
+                kIntakeMotorCurrentLimit);
+        intakeMotor.configure(intakeMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        }
+
+
+
+
+
+        public double getPivotEncoder() {
+            return (-pivotEncoder.getPosition() + absoluteOffset);
+        }
+
+        public double GetEffort() {
+            return pivotFeedForward.calculate(Units.degreesToRadians(getPivotEncoder()), 0)
+            + pivotController.calculate(getPivotEncoder(), pivotSetPoint);
+        }
+
+        public double simGetEffort() {
+            return simPivotFeedForward.calculate(Units.degreesToRadians(pivotViz.getAngle()), 0) 
+            + simPivotController.calculate(simEncoder, pivotSetPoint);
+        }
+
+
+
+
+
+        @Override
+        public void periodic() {
+            encoderValue = getPivotEncoder();
+            SmartDashboard.putNumber("pivotEncoder", encoderValue);
+            SmartDashboard.putNumber("Intake Motor Output", intakeMotor.getAppliedOutput());
+            SmartDashboard.putNumber("Pivot Motor Output", pivotMotor.getAppliedOutput());
+            if (Utils.isSimulation()) {
+                simEncoder = pivotViz.getAngle();
+                pivotSim.setInput(simGetEffort());
+                pivotSim.update(0.01);
+                pivotViz.setAngle(Units.radiansToDegrees(pivotSim.getAngleRads()));
+
+
+            }
+        }
+
 
     }
 
-}
+   
+
+
+
